ad1ea72b9f0873fdcd125f4badfe2487e41da508
wendixue17 <wendixue17@gmail.com>
2025-11-19 16:04:36 -0500

    top 25 stocks and check small-cap

--- base code.py
+++ base code.py
@@ -14,16 +14,21 @@ from datetime import datetime, timedelta
 
 # Load the tickers
 df = pd.read_csv('Tickers_Example.csv', header=None) # test with Tickers_Example.csv?
+# If there are any duplicate tickers, we will drop them
+df = df.drop_duplicates()
 ticker_lst = list(df.iloc[:,0])
 
 # Filter out tickers that don't exist; include only valid tickers
 valid_tickers_lst = []
-for i in range(len(ticker_lst)):
-    ticker_data = yf.Ticker(ticker_lst[i]) # call up data
-    country = ticker_data.info.get('country') 
-    currency = ticker_data.info.get('currency')
-    if country in {'Canada','United States'} and currency in {'CAD','USD'}: # filters us and canadian tickers and listed stocks
-        valid_tickers_lst.append(ticker_lst[i])
+for ticker in ticker_lst:
+    try: 
+        ticker_data = yf.Ticker(ticker) # call up data
+        country = ticker_data.info.get('country') 
+        currency = ticker_data.info.get('currency')
+        if country in {'Canada','United States'} and currency in {'CAD','USD'}: # filters us and canadian tickers and listed stocks
+            valid_tickers_lst.append(ticker)
+    except Exception as err:
+        print(f"Error: {err}")
 
 print(valid_tickers_lst)
 
@@ -32,9 +37,10 @@ print(valid_tickers_lst)
 volume_start_date = '2024-10-01'
 volume_end_date = '2025-09-30' 
 filtered_lst = []
-for i in range(len(valid_tickers_lst)): # goes through every ticker to filter
+
+for ticker in valid_tickers_lst: # goes through every ticker to filter
     data = yf.download(
-        tickers=valid_tickers_lst[i],
+        tickers=ticker,
         start=volume_start_date,
         end=volume_end_date
     )
@@ -43,12 +49,14 @@ for i in range(len(valid_tickers_lst)): # goes through every ticker to filter
     keep_months = pd.DataFrame() # df of all the months with more than 18 trading days
     volume_data['Month'] = volume_data.index.to_period('M') # create new column of index only by (YYYY-MM)
     grouped_month_index = volume_data.groupby(['Month']) # group data by month
+
     for month, group in grouped_month_index:
         if len(group) >= 18: # if the month has more than 18 trading days
             keep_months = pd.concat([keep_months, group]) # add data of months with more than 18 trading days
-    average_daily_volume = keep_months['Volume'][valid_tickers_lst[i]].mean() # calculate average daily volume
+
+    average_daily_volume = keep_months['Volume'][ticker].mean() # calculate average daily volume
     if average_daily_volume >= 5000: # determine if above or below 5000 shares
-        filtered_lst.append(valid_tickers_lst[i]) # add to filtered list if greater or equal to 5000 shares
+        filtered_lst.append(ticker) # add to filtered list if greater or equal to 5000 shares
 
 print(filtered_lst)
 
@@ -200,13 +208,56 @@ for sec, tics in sector_dict.items():
     print(f"{sec}: {tics}")
 
 # Take the top 5 from each sector (based on their score /100) and put them in a new dataframe
-# Wendi
+top5_count = 0
+sector_top5 = pd.DataFrame() # top 5 tickers of a sector with the best scoring
+def sector_top5(df,sector):
+    for ticker in df['Ticker'].tolist():
+        if df.loc[ticker,'Sector'] == sector:
+            top5_count += 1
+            if top5_count > 5:
+                top5_count = 5
+                smallest_score = sector_top5['Score'].min()
+                smallest_score_index = df.index(smallest_score)
+                if df.loc[ticker,'Score'] > smallest_score:
+                    sector_top5.loc[smallest_score_index] = df[ticker]
+            else:
+                sector_top5 = pd.concat([sector_top5,df[ticker]])
+
+sector_lst = df['Sector'].unique # creates a list of all the sectors
+all_sectors_top5 = pd.DataFrame()
+for sector in sector_lst:
+    df_sector_top5 = sector_top5(df,sector)
+    all_sectors_top5 = pd.concat([all_sectors_top5,df_sector_top5])
 
 # Then return the top 25
-# Wendi
+# sort stocks from highest score to lowest
+all_sectors_top5 = all_sectors_top5.sort_values('Score', ascending=False)
+top_25_stocks = all_sectors_top5.head(25) # top 25 stocks
 
 # Make the LAST ONE A SMALL CAP GUARANTEED
-# Wendi
+# check for small cap in the last row of the top 25 stocks as code determining weight of portfolio will consider this
+small_cap = 2e9
+last_row_market_cap = top_25_stocks['MarketCap'].iloc[-1]
+market_cap = top_25_stocks['MarketCap']
+if last_row_market_cap < small_cap:
+    print("Small cap in the 25th row found")
+# move small cap to the last row if there is a small cap in the top 25 stocks
+elif (market_cap < small_cap).any():
+    for row in top_25_stocks.itertuples():
+        if row.MarketCap < small_cap:
+            top_25_stocks = top_25_stocks.drop(row)
+            top_25_stocks = pd.concat([top_25_stocks,row])
+# get the best scoring small cap from the dataframe of the top 5 of all sectors and replace it with the stock of the last row of the top 25 stocks            
+else:
+    small_cap_df = pd.DataFrame()
+    for row in all_sectors_top5.itertuples(): # change to metrics dataframe
+        if row.MarketCap < small_cap:
+            small_cap_df = pd.concat(small_cap_df,row)
+        break
+    small_cap_df = small_cap_df.sort_values('Score', ascending=False)
+    top_score_small_cap = small_cap_df.iloc[0]
+    # replace the last stock of the top 25 with the small cap with the best score
+    top_25_stocks.iloc[-1] = top_score_small_cap
 
 # Use minimum variance portfolio optimization to determine optimal weights per stock
 
